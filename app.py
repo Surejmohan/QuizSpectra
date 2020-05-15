b3abebddce78928b570347709f64c6833d126a8e
Surej Mohan <soorajmohan121997@gmail.com>
2020-05-15 18:37:11 +0530

    last

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,flash, redirect,url_for,session,logging,request,Response
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm 
 
+#update Other set Score = -1 Where id = 1;
 
 
 app = Flask(__name__)
@@ -153,11 +153,12 @@ def Quiz_start():
     if "user" in session:
         User = session["user"]
         quiz = Quiz.query.filter_by(Quest_No = 1).first()
+        user = Other.query.filter_by(Username = session["user"]).first()
+        session["no"] = 0
         if(quiz == None):
             session.clear()
             flash('Some thing has happend !You can take the Quiz after Sometime','error')
             return redirect(url_for('index'))
-        user = Other.query.filter_by(Username = session["user"]).first()
         if(user.Score == -1):
             user.Score = 0
             db.session.add(user)
@@ -166,7 +167,7 @@ def Quiz_start():
             return render_template('quiz.html',User= User,quiz=quiz)
         else:
             session.clear()
-            flash('You have  Completed Your Quiz! or Get Time Out','error')
+            flash('You have  Completed Your Quiz! or Get Timed Out','error')
         
             return redirect(url_for('index'))
     else:
@@ -178,35 +179,42 @@ def Quiz_start():
 @app.route('/quiz/next',methods=['POST'])
 def Quiz_next():
     
-    if "user" in session:
+    if "user" in session and "no" in session:
         User = session["user"]
         if request.method == 'POST':     
             questno = request.form['questno']
             ans = request.form['ans']
+            if session["no"] < int(questno):
+                print(ans)
+                if(int(questno) == 10):
+                    session.clear()
+                    flash('You have Finished Your Quiz Successfully','success')
+                    return redirect(url_for('index'))
+                check = Quiz.query.filter_by(Quest_No = questno).first()
+                print(check.Answer)
+                if(check.Answer == int(ans)):
+                    user = Other.query.filter_by(Username = session["user"]).first()
+                    user.Score = user.Score + 1
+                    db.session.add(user)
+                    db.session.commit()
+                quiz = Quiz.query.filter_by(Quest_No = int(questno)+1).first()
+                if(quiz == None):
+                    erro = Other.query.filter_by(Username = session["user"]).first()
+                    erro.Score = -1
+                    db.session.add(erro)
+                    db.session.commit()
+                    session.clear()
+                    flash('Some thing has happend !You can take the Quiz after Sometime','error')
+                    return redirect(url_for('index'))
+                session["no"] = int(questno)
+                print(session["no"])
+                print(quiz)
+                return render_template('quiz.html',User= User,quiz=quiz)
+            else:
+                session.clear()
+                flash('You click back button or  Get Timed Out','error')
+                return redirect(url_for('index'))
 
-            print(ans)
-            if(int(questno) == 10):
-                session.clear()
-                flash('You have Finished Your Quiz Successfully','success')
-                return redirect(url_for('index'))
-            check = Quiz.query.filter_by(Quest_No = questno).first()
-            print(check.Answer)
-            if(check.Answer == int(ans)):
-                user = Other.query.filter_by(Username = session["user"]).first()
-                user.Score = user.Score + 1
-                db.session.add(user)
-                db.session.commit()
-            quiz = Quiz.query.filter_by(Quest_No = int(questno)+1).first()
-            if(quiz == None):
-                erro = Other.query.filter_by(Username = session["user"]).first()
-                erro.Score = -1
-                db.session.add(erro)
-                db.session.commit()
-                session.clear()
-                flash('Some thing has happend !You can take the Quiz after Sometime','error')
-                return redirect(url_for('index'))
-            print(quiz)
-            return render_template('quiz.html',User= User,quiz=quiz)
     else:
         flash('Invalid Access','error')
         return redirect(url_for('index'))
@@ -217,7 +225,10 @@ def Quiz_next():
 def Admindashboard():
     if "admin" in session:
         User = session["admin"]
-        return render_template('admin.html',User=User)
+        list1 = db.session.query(Other).all()
+        quests = db.session.query(Quiz).all()
+        print(list1)
+        return render_template('admin.html',User=User,list1=list1,quests = quests)
     else:
         flash('Invalid Access','error')
         return redirect(url_for('index'))
@@ -246,7 +257,28 @@ def AddQuiz():
         return redirect(url_for('index'))
 
 
-    
+
+
+
+@app.route('/Admin/delete/<string:id>/')
+def UserDelete(id):
+    if "admin" in session:
+        us = Other.query.filter_by(id = id).first()
+        print(id)
+        print(us.Username)
+        delete1 = db.session.query(Other).filter(Other.Username == us.Username).first()
+        delete2 = db.session.query(User).filter(User.Username == us.Username).first()
+        db.session.delete(delete2)
+        db.session.delete(delete1)
+        db.session.commit()
+        flash('User is  Successfully Deleted','success')
+        return redirect(url_for('Admindashboard'))
+        
+    else:
+        flash('Invalid Access','error')
+        return redirect(url_for('index'))
+
+
 
 
 if(__name__ == "__main__"):
